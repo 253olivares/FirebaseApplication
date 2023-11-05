bcde49307cc15d91fc4d2139e45f50a04e9748cf
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2023-11-05 18:07:17 -0500

    Alot of stuff
    -Firebase Integration
    -added login and user creation
    -troubleshooting to make sure not issues occur
    -added an additional account button to view current user logged in info
    -integrated cart to update when user logs in so now when user logs in cart will grab user info on what they have saved

--- public/app/app.js
+++ public/app/app.js
@@ -3,6 +3,8 @@ var bannerAd = true;
 let emptyCartPage = `<p>0 ITEM</p>
     <h1>You don't have any items in your shopping cart</h1>`;
 var topNav;
+var loginStatus = false;
+var loggedUser;
 
 var coffeeCart = [];
 
@@ -43,10 +45,10 @@ let route = () => {
         default:
             if (pageID.length == 0) {
                 MODEL.changeContent("home", afterRoute);
-                console.log("First load blank");
+                console.log("route: First load blank");
             } else {
                 MODEL.changeContent("404", afterRoute);
-                console.log("url us invalid or empty taken back to home");
+                console.log("route: url us invalid or empty taken back to home");
             }
             break
     }
@@ -54,14 +56,13 @@ let route = () => {
 
 // function to run any script after redirection to a new page
 let afterRoute = (page) => {
-
     switch (page) {
         case "home":
-            console.log("You are on the home page!");
+            console.log("afterRoute: You are on the home page!");
             loadMachine();
             break;
         case "cart":
-            console.log("you are on the cart page!");
+            console.log("afterRoute: you are on the cart page!");
             checkCartPage();
             break;
     }
@@ -79,7 +80,7 @@ let checkBanner = () => {
             sessionStorage.setItem("banner", true);
             bannerAd = true;
         })
-        console.log("no local storeage banner!");
+        console.log("checkBanner function: no local storeage banner!");
     }
 }
 
@@ -89,13 +90,14 @@ let setFooterHeight = () => {
 }
 
 let cartNumer = () => {
+    console.log("cartNumber:Running");
     try {
         if (coffeeCart.length >= 1) {
-            console.log("array has more than one");
+            console.log("cartnumber functution has run and has found array has more than one");
             $(".keurigHeader__topNav__extraRight__cart__count").css("display", "inline-block");
             $(".keurigHeader__topNav__extraRight__cart__count").html(coffeeCart.length);
         } else {
-            console.log("arrary has none");
+            console.log("cartnumber functution has run and has found arrary has none");
             $(".keurigHeader__topNav__extraRight__cart__count").css("display", "none");
         }
     } catch {
@@ -168,7 +170,7 @@ let loadMachine = () => {
         .get()
         .then(
             (x) => {
-                console.log(x);
+                console.log("loadMachine function has ran. Database has found and returned: ", x);
                 x.forEach((y) => {
                     // Holders
 
@@ -261,7 +263,7 @@ let loadMachine = () => {
 
 // Minor function that counts number of coffee items that exist on the data base and displays number of coffee list page
 let addCoffeeCount = () => {
-    console.log($("#coffeeDisplay > div").length);
+    console.log("addCoffeeCount function has run. Output: ", $("#coffeeDisplay > div").length);
     $(".coffeeMakerCount").append($("#coffeeDisplay > div").length);;
 }
 
@@ -278,7 +280,7 @@ let checkColorListener = () => {
 
 let addCoffee2Cart = (id) => {
     let targetItem = $(event.target).parent().parent()
-    console.log(targetItem.attr("listNum"));
+    console.log("addCoffee2Cart page has ran. Output: ", targetItem.attr("listNum"));
     if (sessionStorage.login == false) {
 
     } else {
@@ -295,41 +297,82 @@ let checkCartPage = () => {
     }
 }
 
+let accountInfoHover = () => {
+    let click = false;
+    $(".keurigHeader__topNav__extraRight__account, .accountContainer").hover(() => {
+        if (bannerAd == true) {
+            let top = $(".bannerAd").height() + $(".keurigHeader__topNav").height() - 5 - parseInt($(".keurigHeader__topNav__extraRight__profile").css("padding-bottom"));
+            $(".grayOverlay ").css({ "display": "block", "top": top, "height": `calc(100% - ${top}px)` });
+            $(".accountContainer").css({ "display": "block", "top": top, });
+            $('html, body').css({
+                overflow: 'hidden'
+            });
+            loadInfo();
+        } else {
+            let top = $(".keurigHeader__topNav").height() - parseInt($(".keurigHeader__topNav__extraRight__profile").css("padding-bottom") - 5);
+            $(".grayOverlay").css({ "display": "block", "top": top, "height": `calc(100% - ${top}px)` });
+            $(".accountContainer").css({ "display": "block", "top": top, });
+            $('html, body').css({
+                overflow: 'hidden'
+            });
+            loadInfo();
+        }
+        $(".keurigHeader__topNav__extraRight__account ").click(() => {
+            click = true;
+        })
+        $(".accountContainer__close > i, .accountContainer__close > p,.grayOverlay").click(() => {
+            click = false;
+            $(".grayOverlay").css({ "display": "none" });
+            $(".accountContainer").css({ "display": "none" });
+            $('html, body').css({
+                overflow: 'auto'
+            });
+        })
+    }, () => {
+        if (click == false) {
+            $(".grayOverlay").css({ "display": "none" });
+            $(".accountContainer").css({ "display": "none" });
+            $('html, body').css({
+                overflow: 'auto'
+            });
+        }
+    });
+}
+
+let loadInfo = () => {
+    $(".accountContainer__info__span").text(loggedUser.fname, " ", loggedUser.lname);
+    $(".accountContainer__info__email").text(loggedUser.email);
+    $(".accountContainer__info__uid").text(loggedUser.id);
+    $(".accountContainer__info__specials").text(loggedUser.specials);
+}
+
 // function that takes care of loding and hidding the gray overlay the pops up when user tried to sign in or access search bar
 let accountHover = () => {
     let click = false;
     $(".keurigHeader__topNav__extraRight__profile,.loginContainer ").hover(() => {
         if (bannerAd == true) {
             let top = $(".bannerAd").height() + $(".keurigHeader__topNav").height() - 5 - parseInt($(".keurigHeader__topNav__extraRight__profile").css("padding-bottom"));
-            console.log("Banner Height:", $(".bannerAd").height());
-            console.log("Top Nav Height:", $(".keurigHeader__topNav").height());
-            console.log("Padding Height:", parseInt($(".keurigHeader__topNav__extraRight__profile").css("padding-bottom")));
             $(".grayOverlay ").css({ "display": "block", "top": top, "height": `calc(100% - ${top}px)` });
             $(".loginContainer").css({ "display": "block", "top": top, });
             $('html, body').css({
                 overflow: 'hidden'
             });
-            accountModal();
 
             console.log(1);
         } else {
             let top = $(".keurigHeader__topNav").height() - parseInt($(".keurigHeader__topNav__extraRight__profile").css("padding-bottom") - 5);
-            console.log("Banner Height:", $(".bannerAd").height());
-            console.log("Top Nav Height:", $(".keurigHeader__topNav").height());
-            console.log("Padding Height:", parseInt($(".keurigHeader__topNav__extraRight__profile").css("padding-bottom")));
             $(".grayOverlay").css({ "display": "block", "top": top, "height": `calc(100% - ${top}px)` });
             $(".loginContainer").css({ "display": "block", "top": top, });
             $('html, body').css({
                 overflow: 'hidden'
             });
-            accountModal();
 
             console.log(2);
         }
         $(".keurigHeader__topNav__extraRight__profile,.inputEnter,.chec ").click(() => {
             click = true;
         })
-        $(".loginContainer__close > i, .loginContainer__close > p,.grayOverlay ").click(() => {
+        $(".loginContainer__close > i, .loginContainer__close > p,.grayOverlay ,.loginContainer__formsSignup__signUp,.loginContainer__formsLogin__signIn ").click(() => {
             click = false;
             $(".grayOverlay").css({ "display": "none" });
             $(".loginContainer").css({ "display": "none" });
@@ -350,10 +393,22 @@ let accountHover = () => {
     })
 
 }
+
+let signoutClick = () => {
+    $(".keurigHeader__topNav__extraRight__signOut").click(() => {
+        firebase.auth().signOut().then(() => {
+            console.log("signoutClick: Signed Out");
+            loginStatus = false;
+            MODEL.changeContent("home", afterRoute);
+        })
+            .catch((error) => {
+                console.log("signoutClick Error Occured:", error);
+            })
+    });
+}
 // Modal create for user to create a new account using an email and password
 let accountModal = () => {
     $(".loginContainer__options__logIn, .loginContainer__options__signUp").click(() => {
-        console.log(event.target);
         $(event.target).parent().children().removeClass("lCSelected")
         $(event.target).addClass("lCSelected");
         let value = $(event.target).attr("value")
@@ -374,19 +429,227 @@ let accountModal = () => {
         }
     });
 }
+// A function that contains a list of functions I want running before hashing the page
+let commands2LoadBeforePageHash = () => {
+    debugCommands();
+    accountHover();
+    accountInfoHover();
+    signoutClick();
+    searchBarListener();
+    checkBanner();
+    setFooterHeight();
+}
+
+// function to sign up
+let signup = () => {
+    let fname = $("#fname-Signup").val().trim();
+    let lname = $("#lname-Signup").val().trim();
+    let email = $("#email-Signup").val().trim();
+    let password = $("#password-Signup").val().trim();
+    let specials = $("#checkboxSpecials").is(':checked');
+    let remember = $("#checkboxKeep").is(':checked');
+    console.log(fname, fname, email, password, specials, remember);
+
+    if (remember == true) {
+        firebase
+            .auth()
+            .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+            .then(() => {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then((userCredential) => {
+                        $("#fname-Signup").val("");
+                        $("#lname-Signup").val("");
+                        $("#email-Signup").val("");
+                        $("#password-Signup").val("");
+                        _db
+                            .collection("USERS")
+                            .doc(`${userCredential.user.uid}`)
+                            .set({
+                                id: userCredential.user.uid,
+                                fname: fname,
+                                lname: lname,
+                                email: email,
+                                password: password,
+                                specials: specials,
+                                cart: []
+                            })
+                            .then(() => {
+                                console.log("Signup: Document has successfully been written!");
+                                loginStatus = true;
+                            })
+                            .catch((error) => {
+                                console.error("SignUp: Error writing document: ", error);
+                            });
+                        console.log(userCredential.user);
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorMessage);
+                        alert(errorCode + " " + errorMessage);
+                    });
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            })
+    } else {
+        firebase
+            .auth()
+            .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(() => {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(email, password)
+                    .then((userCredential) => {
+                        $("#fname-Signup").val("");
+                        $("#lname-Signup").val("");
+                        $("#email-Signup").val("");
+                        $("#password-Signup").val("");
+                        _db
+                            .collection("USERS")
+                            .doc(`${userCredential.user.uid}`)
+                            .set({
+                                id: userCredential.user.uid,
+                                fname: fname,
+                                lname: lname,
+                                email: email,
+                                password: password,
+                                specials: specials,
+                                cart: []
+                            })
+                            .then(() => {
+                                console.log("Signup: Document has successfully been written!");
+                                loginStatus = true;
+                            })
+                            .catch((error) => {
+                                console.error("SignUp: Error writing document: ", error);
+                            });
+                        console.log(userCredential.user);
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorMessage);
+                        alert(errorCode + " " + errorMessage);
+                    });
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            })
+    }
+}
+
+let login = () => {
+    let email = $("#email-login").val();
+    let pass = $("#password-Login").val();
+    let remember = $("#checkboxRemeber").is(':checked');
+
+    if (remember == true) {
+        firebase
+            .auth()
+            .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+            .then(() => {
+                return firebase
+                    .auth()
+                    .signInWithEmailAndPassword(email, pass)
+                    .then(() => {
+                        $("#email-login").val("")
+                        $("#password-Login").val("")
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorMessage);
+                        alert(errorCode + " " + errorMessage);
+                    });
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            })
+    } else {
+        firebase
+            .auth()
+            .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(() => {
+                return firebase
+                    .auth()
+                    .signInWithEmailAndPassword(email, pass)
+                    .then(() => {
+                        $("#email-login").val("")
+                        $("#password-Login").val("")
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        console.log(errorMessage);
+                        alert(errorCode + " " + errorMessage);
+                    });
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            })
+    }
+}
+
+// Firebase command that will run at start to see if user credentals already exist on the web browser. If so to store them into local storage.
+let initFirebase = (callback) => {
+    commands2LoadBeforePageHash();
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            console.log("User is logged in");
+            _db.collection("USERS")
+                .where("email", "==", user.email)
+                .get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        loginStatus = true;
+                        loggedUser = doc.data();
+                        coffeeCart = doc.data().cart;
+                        console.log("initFirebase", loginStatus);
+                        console.log("initFirebase", loggedUser);
+                        cartNumer();
+                    });
+                }).catch((error) => {
+                    console.log("Error getting documents: ", error);
+                })
+            $(".keurigHeader__topNav__extraRight__signOut").css("display", "flex");
+            $(".keurigHeader__topNav__extraRight__account").css("display", "flex");
+            $(".keurigHeader__topNav__extraRight__profile").css("display", "none");
+
+        } else {
+            console.log("User is not logged in");
+            $(".keurigHeader__topNav__extraRight__signOut").css("display", "none");
+            $(".keurigHeader__topNav__extraRight__account").css("display", "none");
+            $(".keurigHeader__topNav__extraRight__profile").css("display", "flex");
+            loginStatus = false;
+            loggedUser = null;
+            coffeeCart = [];
+            cartNumer();
+        }
+    })
+    callback();
+}
 
 // functions that will run at the start of out website. As soon as the dom loads all the intial functions begin to run.
 $(document).ready(() => {
     try {
         let app = firebase.app();
         _db = firebase.firestore();
-        debugCommands();
-        checkHash();
-        setFooterHeight();
-        accountHover();
-        searchBarListener();
-        cartNumer();
-        checkBanner();
+        initFirebase(checkHash)
     } catch (e) {
         console.error("Document.ready has failed Error: " + e);
         alert(e);
